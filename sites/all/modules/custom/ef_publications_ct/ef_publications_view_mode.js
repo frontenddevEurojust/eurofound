462e6536066661a2ead643d20891afcda0ff1cb1
Markel <marauzo@bilbomatica.es>
2017-05-24 16:40:51 +0200

    Remove forthcoming tab from breadcrumb

---
+++ sites/all/modules/custom/ef_publications_ct/ef_publications_view_mode.js
@@ -0,0 +1,9 @@
+
+(function($) {
+    $(document).ready(function(){
+		
+		var title = $('.breadcrumbs .current').text().replace('Forthcoming','');
+		$('.breadcrumbs .current a').text(title);
+
+	});
+})(jQuery)
