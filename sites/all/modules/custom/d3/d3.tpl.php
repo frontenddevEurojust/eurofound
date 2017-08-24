f81c88992d44e9d8895c99588d07db9517c037c7
ilorenzo <jilorenzo@bilbomatica.es>
2017-08-24 15:21:08 +0200

    footer and back button

--- sites/all/modules/custom/d3/d3.tpl.php
+++ sites/all/modules/custom/d3/d3.tpl.php
@@ -9,6 +9,7 @@ drupal_add_js(drupal_get_path('module', 'ef_d3_dataexplorer') . '/charts/d3.ejm/
 
 
 <div  class="jm-charts-wrapper ">	
+
 	<div class="row">
 		<div class="jm-abstract-wrapper large-9">
 		  <h1 id='pagetitle' class='title'><?php print drupal_get_title(); ?></h1>
@@ -20,10 +21,11 @@ drupal_add_js(drupal_get_path('module', 'ef_d3_dataexplorer') . '/charts/d3.ejm/
 		</div>
 		<div class="jm-back-button large-3">
 			<section class="block block-block boxed-block back-to-results-block block-block-13 clearfix">
-				<a href="#" title="Back to Data Explorer">Data Explorer</a>  
+				<a href="/data-explorer" title="Back to Data Explorer">Data Explorer</a>  
 			</section>
 		</div>
 	</div>
+
 	<div class="jm-filters-chart">
 		<div class="filters-jm-chart large-3">
 			<form>
@@ -55,11 +57,11 @@ drupal_add_js(drupal_get_path('module', 'ef_d3_dataexplorer') . '/charts/d3.ejm/
 			  </fieldset>
 			</form>		
 		</div>
+
 		<div class="jm-charts large-9 <?php print implode(' ', $classes_array); ?>">
-			<h2>Employment shifts by <span class="criterion"></span> quintile (and by  <span class="breakdown"></span>), for <span class="country"></span>, <span class="period"></span></h2>
+				<h2>Employment shifts by <span class="criterion"></span> quintile (and by  <span class="breakdown"></span>), for <span class="country"></span>, <span class="period"></span></h2>
 				<div id="ejm-chart"></div>
 				<div class="jm-footnote"></div>
-			</div>
 		</div>
 	</div>
 	
@@ -71,4 +73,5 @@ drupal_add_js(drupal_get_path('module', 'ef_d3_dataexplorer') . '/charts/d3.ejm/
 			<p>Morbi dui tellus, pulvinar maximus magna sed, consectetur consequat risus. Nam vestibulum est in molestie elementum. Morbi gravida nisl nec sem consectetur dignissim. Donec scelerisque sem accumsan massa lacinia viverra. Phasellus vel purus bibendum, mollis nisl convallis, ultrices elit. Sed eu tortor placerat turpis blandit suscipit. Morbi vel turpis sapien. Nam sodales metus eu purus condimentum consequat. Etiam posuere tempus volutpat. Praesent nec dapibus odio. Etiam tempus urna quis odio egestas, id ullamcorper ex sagittis.</p>		
 		</div>
 	</div>
+
 </div>
