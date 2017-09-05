a5c09760b2fdc475592007db37e504c1008c7cf4
MAREN LARREA <mlarrea@bilbomatica.es>
2017-09-05 10:22:59 +0200

    WEM-684: refactor Jobs monitor database and data visualisation Changed code structure

--- sites/all/modules/custom/ef_d3_dataexplorer/charts/d3.ejm/ejm.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  $(document).ready(function(){
-		$(".filters-jm-chart fieldset legend").click(function() {
-			 $(this).parent().find("div.group-filters").toggle();
-			 $(this).toggleClass('opened');
-		});
-  });
-})(jQuery);
