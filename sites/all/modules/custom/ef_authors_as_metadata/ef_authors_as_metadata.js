98f2811d2bac300d4a0cacbff4c688bca20b6f4f
Markel <marauzo@bilbomatica.es>
2017-09-29 15:34:14 +0200

    WEM-539: FIX Authors as metadata usability fixes
    - Add validation for textfield (now controls empty values)
    - Onkeypress event for textfield
    - Only one message is shown regardless of the type (error or succes)

--- sites/all/modules/custom/ef_authors_as_metadata/ef_authors_as_metadata.js
+++ sites/all/modules/custom/ef_authors_as_metadata/ef_authors_as_metadata.js
@@ -2,31 +2,30 @@
     $(document).ready(function(){
 
 
-	        var textPublContrib = jQuery('#edit-field-ef-publ-contributors .description span').attr('title');
+	        var textPublContrib = $('#edit-field-ef-publ-contributors .description span').attr('title');
             
             if(textPublContrib == undefined){
-                var textPublContrib = jQuery('#edit-field-ef-publ-contributors .description').text();
+                var textPublContrib = $('#edit-field-ef-publ-contributors .description').text();
             }
 	    
 
-            jQuery('#edit-field-ef-publ-contributors .description').css('display','none');
+            $('#edit-field-ef-publ-contributors .description').css('display','none');
 
-            jQuery('.form-item-field-ef-publ-contributors-und label').after('<p>' + textPublContrib + '</p>');
+            $('.form-item-field-ef-publ-contributors-und label').after('<p>' + textPublContrib + '</p>');
 
 	        
 
-	        var textNewAuthor = jQuery('#edit-add-new-contributor-wrapper .description span').attr('title');
+	        var textNewAuthor = $('#edit-add-new-contributor-wrapper .description span').attr('title');
 
-            if(jQuery('#edit-add-new-contributor-wrapper .description') != ''){               
+            if($('#edit-add-new-contributor-wrapper .description') != ''){               
                 if(textNewAuthor == undefined){
-                    var textNewAuthor = jQuery('#edit-add-new-contributor-wrapper .description').text();
+                    var textNewAuthor = $('#edit-add-new-contributor-wrapper .description').text();
                 }     
             }
 	        
 
-            jQuery('#edit-add-new-contributor-wrapper .description').css('display','none');
+            $('#edit-add-new-contributor-wrapper .description').css('display','none');
 
-            jQuery('.form-item-add-new-contributor label').after('<p>' + textNewAuthor + '</p>');
-  	
+            $('.form-item-add-new-contributor label').after('<p>' + textNewAuthor + '</p>');  	
     });
 })(jQuery)
--- sites/all/modules/custom/ef_authors_as_metadata/ef_authors_as_metadata_error.js
+++ sites/all/modules/custom/ef_authors_as_metadata/ef_authors_as_metadata_error.js
@@ -1,10 +1,12 @@
 (function($, Drupal)
 {
 	// Our function name is prototyped as part of the Drupal.ajax namespace, adding to the commands:
-	Drupal.ajax.prototype.commands.Error = function(ajax, response, status)
+	Drupal.ajax.prototype.commands.ErrorAuthorExists = function(ajax, response, status)
 	{
 
-		jQuery('.form-item-add-new-contributor input').after('<span class="error-msg">The author ' + response.error + ' already exists.<a href="javascript:" class="msg-error-close"><i class="fa fa-times" aria-hidden="true"></i></a></span>');
+		RemoveItemsByClass('success-msg', 'error-msg');
+
+		$('.form-item-add-new-contributor input').after('<span class="error-msg">The author ' + response.error + ' already exists.<a href="javascript:" class="msg-error-close"><i class="fa fa-times" aria-hidden="true"></i></a></span>');
 
 		$('.msg-error-close').click(function(){
 
@@ -12,4 +14,29 @@
 		});
 
 	};
+
+		// Our function name is prototyped as part of the Drupal.ajax namespace, adding to the commands:
+	Drupal.ajax.prototype.commands.EmptyValue = function(ajax, response, status)
+	{
+
+		RemoveItemsByClass('success-msg', 'error-msg');
+
+		$('.form-item-add-new-contributor input').after('<span class="error-msg">Something needs to be introduced for saving.<a href="javascript:" class="msg-error-close"><i class="fa fa-times" aria-hidden="true"></i></a></span>');
+
+		$('.msg-error-close').click(function(){
+
+			console.log($(this));
+
+			$(this).parent().remove();
+		});
+
+	};
 }(jQuery, Drupal));
+
+
+function RemoveItemsByClass(){
+    
+    for (var i=0; i < arguments.length; i++){
+        jQuery('.' + arguments[i]).remove();
+    }
+}
