7b4aa574c1b78df7e574d83e040507ac544b7804
iarias <iarias@bilbomatica.es>
2017-10-06 11:43:03 +0200

    WEM-683 feat Blog and Presentations Fix responsive email ico

--- sites/all/themes/effoundationtheme/js/blog-presentation.js
+++ sites/all/themes/effoundationtheme/js/blog-presentation.js
@@ -1,28 +1,33 @@
-jQuery( document ).ready(function() {
+(function($) {
+    $(document).ready(function(){
+             // Add span to title for decrease the width
+        $( "#page-title" ).wrapInner( "<span class='title-blog-presentation'></span>" );
 
-    // Add span to title for decrease the width
-    jQuery( "#page-title" ).wrapInner( "<span class='title-blog-presentation'></span>" );
+        //Editing service links CSS
+        $( ".block-easy-social-easy-social-block-2 .easy_social-widget-facebook i" ).removeClass( "fa-facebook-square" );
+        $( ".block-easy-social-easy-social-block-2 .easy_social-widget-facebook i" ).addClass( "fa-facebook" );
 
-    //Editing service links CSS
-    jQuery( ".block-easy-social-easy-social-block-2 .easy_social-widget-facebook i" ).removeClass( "fa-facebook-square" );
-    jQuery( ".block-easy-social-easy-social-block-2 .easy_social-widget-facebook i" ).addClass( "fa-facebook" );
+        $( ".block-easy-social-easy-social-block-2 .easy_social-widget-twitter i.fa" ).removeClass( "fa-twitter-square" );
+        $( ".block-easy-social-easy-social-block-2 .easy_social-widget-twitter i.fa" ).addClass( "fa-twitter" );
+        
+        $( ".block-easy-social-easy-social-block-2 .easy_social-widget-linkedin i" ).removeClass( "fa-linkedin-square" );
+        $( ".block-easy-social-easy-social-block-2 .easy_social-widget-linkedin i" ).addClass( "fa-linkedin" );
 
-    jQuery( ".block-easy-social-easy-social-block-2 .easy_social-widget-twitter i.fa" ).removeClass( "fa-twitter-square" );
-    jQuery( ".block-easy-social-easy-social-block-2 .easy_social-widget-twitter i.fa" ).addClass( "fa-twitter" );
-    
-    jQuery( ".block-easy-social-easy-social-block-2 .easy_social-widget-linkedin i" ).removeClass( "fa-linkedin-square" );
-    jQuery( ".block-easy-social-easy-social-block-2 .easy_social-widget-linkedin i" ).addClass( "fa-linkedin" );
+        $(".fa-facebook").click(function(){
+            $(".fa-facebook").toggleClass("active");
+        });
 
-    jQuery(".fa-facebook").click(function(){
-        jQuery(".fa-facebook").toggleClass("active");
+        $(".fa-twitter").click(function(){
+            $(".fa-twitter").toggleClass("active");
+        });
+
+        $(".fa-linkedin").click(function(){
+            $(".fa-linkedin").toggleClass("active");
+        });
+
+
+        //Move the email ico above the Block block-easy-social
+        $(".email-blog").appendTo(".easy_social_box");
     });
+})(jQuery)
 
-    jQuery(".fa-twitter").click(function(){
-        jQuery(".fa-twitter").toggleClass("active");
-    });
-
-    jQuery(".fa-linkedin").click(function(){
-        jQuery(".fa-linkedin").toggleClass("active");
-    });
-    
-});
