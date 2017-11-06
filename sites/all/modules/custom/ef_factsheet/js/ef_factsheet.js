1d9702243b50e68249d61ec3508596a674db6f17
iarias <iarias@bilbomatica.es>
2017-11-06 09:30:27 +0100

    WEM-842 feat Extra Data Validation Validation min job and job creation both empty. Validation new location if offshoring/delocalisation is selected

--- sites/all/modules/custom/ef_factsheet/js/ef_factsheet.js
+++ sites/all/modules/custom/ef_factsheet/js/ef_factsheet.js
@@ -17,6 +17,73 @@
       }
 
     });
+
+    //Min job loss or job creation –cannot submit if both are empty 
+
+    
+    //New location: cannot be blank if “offshoring/ delocalisation” is selected in restructuring type
+
+    $( "#edit-field-ef-type-of-restructuring-und" ).change(function() {
+      $('.chosen-container-multi .search-choice span').each(function () {
+          if ($(this).once().text() == 'Offshoring/Delocalisation') {
+              if ($('#edit_field_ef_new_location_und_chosen .chosen-single span').text() == '- None -') {
+                $('#edit-submit').attr("disabled","disabled");
+                $('#edit-save-edit').attr("disabled","disabled");
+
+                $("#edit-field-ef-new-location div label").once().append("<div id='alertLocation' class='reveal-modal' class='reveal-modal' data-reveal='' aria-labelledby='modalTitle' aria-hidden='false' role='dialog'><p class='lead'>New location cannot be none if 'offshoring/delocalisation' is selected in Type of Restructuring</p><a id='accept-button' class='btn-payment'>Accept</a></div></div>" );
+                $("body").once().append("<div class='reveal-modal-bg payment' style='display: block;'></div>");
+                $('#alertLocation').show();
+                $('.reveal-modal-bg').show();
+
+
+                $('#accept-button').click(function() {
+                  $('#alertLocation').hide();
+                  $('.reveal-modal-bg').hide();
+                });
+                $('#go-to').click(function() {
+                  $('#alertLocation').hide();
+                  $('.reveal-modal-bg').hide();
+                  $('#edit-field-ef-approved-for-payment-und-0-value-datepicker-popup-0').focus().click();
+                });
+
+              }
+          }else{
+            $('#edit-submit').removeAttr("disabled");
+            $('#edit-save-edit').removeAttr("disabled","disabled");
+          }
+      });
+    });
+
+    $( "#edit-field-ef-new-location-und" ).change(function() {
+
+      $('.chosen-single span').each(function () {
+          if ($(this).text() == '- None -') {
+              if ($('#edit_field_ef_type_of_restructuring_und_chosen .search-choice span').text() == 'Offshoring/Delocalisation') {
+                $('#edit-submit').attr("disabled","disabled");
+                $('#edit-save-edit').attr("disabled","disabled");
+                $("#edit-field-ef-new-location div label").once().append("<div id='alertLocation' class='reveal-modal' class='reveal-modal' data-reveal='' aria-labelledby='modalTitle' aria-hidden='false' role='dialog'><p class='lead'>New location cannot be none if 'offshoring/delocalisation' is selected in Type of Restructuring</p><a id='accept-button' class='btn-payment'>Accept</a></div></div>" );
+                $("body").once().append("<div class='reveal-modal-bg payment' style='display: block;'></div>");
+                $('#alertLocation').show();
+                $('.reveal-modal-bg').show();
+
+
+                $('#accept-button').click(function() {
+                  $('#alertLocation').hide();
+                  $('.reveal-modal-bg').hide();
+                });
+                $('#go-to').click(function() {
+                  $('#alertLocation').hide();
+                  $('.reveal-modal-bg').hide();
+                  $('#edit-field-ef-approved-for-payment-und-0-value-datepicker-popup-0').focus().click();
+                });
+                
+               }
+          }else{
+            $('#edit-submit').removeAttr("disabled");
+            $('#edit-save-edit').removeAttr("disabled","disabled");
+          }
+      });
+    });
   });
 })(jQuery);
 
