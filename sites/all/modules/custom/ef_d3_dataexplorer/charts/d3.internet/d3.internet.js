bd22724fa6535407fb317cfb620003041c54ee7d
jilorenzo <jilorenzo@bilbomatica.es>
2018-04-06 08:13:40 +0200

    internet chart sort and xasis

--- sites/all/modules/custom/ef_d3_dataexplorer/charts/d3.internet/d3.internet.js
+++ sites/all/modules/custom/ef_d3_dataexplorer/charts/d3.internet/d3.internet.js
@@ -22,49 +22,43 @@
       return row.modalityCode == modality && row.subgroupCode == subgroup;
     });
 
-    if (sort == 1 || sort == 2) {
-      sort == 1 ? order = d3.ascending : order = d3.descending;
-      var filteredKeyed = d3.nest()
-        .key(function(d) { return d.countryName; }).sortKeys(order)
-        .entries(filtered);
+    if (sort == 0 ) {
+      // sort == 1 ? order = d3.ascending : order = d3.descending;
+      order = d3.ascending;
+      var filteredKeyed = d3.nest().key(function(d) { 
+        if(d.countryName != 'EU28'){
 
-      filtered = filteredKeyed.map(function(a) { return a.values[0];});
+          return d.countryName;
+
+        }else{
+
+          // firts element in the order
+          return 'AAAA'+d.countryName;
+        }; 
+      }).sortKeys(order).entries(filtered);
+
+      filtered = filteredKeyed.map(function(a) { 
+          return a.values[0];     
+      });
+
     }
 
-    if (sort == 3) {
+    if (sort == 1) {
       var byMinValue = filtered.slice(0);
       byMinValue.sort(function(d,b) {
-        return b.dot1 - d.dot1;
+        return d3.descending(+d.dot1,+b.dot1);
       });
       
       filtered = byMinValue;
     }
 
-    if (sort == 4) {
+    if (sort == 2) {
       var byMaxValue = filtered.slice(0);
       byMaxValue.sort(function(d,b) {
-        return b.dot2 - d.dot2;
+        return d3.descending(+d.dot2,+b.dot2);
       });
       
       filtered = byMaxValue;
-    }
-
-    if (sort == 5) {
-      var byValueGap = filtered.slice(0);
-      byValueGap.sort(function(d,b) {
-        return Math.abs(Math.round(d.dot1) - Math.round(d.dot2)) - Math.abs(Math.round(b.dot1) - Math.round(b.dot2));
-      });
-      
-      filtered = byValueGap;
-    }
-
-    if (sort == 6) {
-      var byValueGap = filtered.slice(0);
-      byValueGap.sort(function(d,b) {
-        return Math.abs(Math.round(b.dot1) - Math.round(b.dot2)) - Math.abs(Math.round(d.dot1) - Math.round(d.dot2));
-      });
-      
-      filtered = byValueGap;
     }
 
     return filtered;
@@ -103,7 +97,13 @@
   }
 
   var createOrderingFilter = function() {
-    var alphaSort = ["- None -", "Alphabetically ascending", "Alphabetically descending", "By 2007 value descending", "By 2016 value descending", "By value gap ascending", "By value gap descending"];
+    // var alphaSort = ["- None -", "Alphabetically ascending", "Alphabetically descending", "By 2007 value descending", "By 2016 value descending", "By value gap ascending", "By value gap descending"];
+    var alphaSort = ["Alphabetically ascending (with EU28 first)", "By 2011 value descending", "By 2016 value descending"];
+
+
+
+
+
 
     var select = d3.select('body .chart-filters').append('select').property('id', 'sort-filter').property('name', 'sort');
 
@@ -286,9 +286,10 @@
 
     xAxis.tickFormat(function(d,i) {
         if (i == 0) {
-          return domainMin;
+          var domainMinRound = x.domain()[0];
+          return d3.format(".0%")(domainMinRound/100);
         } else {
-          return d3.format(".2s")(d); 
+          return d3.format(".0%")(d/100); 
         }
     });
   
@@ -378,6 +379,8 @@
         console.log('Requested csv at "/sites/default/files/ejm/data/' + languageCode + '/internet/internet_' + languageCode + '.csv" was not found.');
       }
 
+
+
         // Initialize tooltip
       tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d; });
 
@@ -409,8 +412,8 @@
       
       // Might need to be created with excel data
       var legendLabels = [
-        {label: "(%) - 2007", class: "lollipop-start"}, 
-        {label: "(%) - 2016", class: "lollipop-end"},
+        {label: "(%) level of participation - 2011", class: "lollipop-start"}, 
+        {label: "(%) level of participation - 2016", class: "lollipop-end"},
       ];
       
       var padding = 0;
@@ -477,6 +480,8 @@
 
         var domainMax = Math.round(calculateMaxValue(filteredData) + 1);
         var domainMin = Math.round(calculateMinValue(filteredData) - 1);
+
+        console.log( filteredData );
         
         y = d3.scaleBand()
           .domain(filteredData.map(function(d) { return d.countryName }))
@@ -495,9 +500,10 @@
         xAxis = d3.axisTop().scale(x)
           .tickFormat(function(d,i) {
             if (i == 0) {
-              return domainMin;
+              var domainMinRound = x.domain()[0];
+              return d3.format(".0%")(domainMinRound/100);
             } else {
-              return d3.format(".2s")(d); 
+              return d3.format(".0%")(d/100); 
             }
           });
         
