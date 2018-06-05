674826a209804eee84ddceda02046ce52b3d0acf
MAREN LARREA <mlarrea@bilbomatica.es>
2018-06-05 08:37:24 +0200

    WEM-987 Add SAI label to titles FIX Fixed JS behaviors for this feature, removed unsets

--- sites/all/modules/custom/ef_sai_label/js/ef_sai_label.js
+++ sites/all/modules/custom/ef_sai_label/js/ef_sai_label.js
@@ -4,6 +4,15 @@
     $('input#edit-field-ef-sai-label-override-und').once().change(function(){
       $("input#edit-field-ef-sai-label-en-0-value").prop("disabled", !$(this).is(':checked'));
     });
+
+    $('input#edit-field-ef-observatory-und-13188').once().change(function(){
+      if ($("input#edit-field-ef-observatory-und-13188").is(':checked')) {
+        $(".group-ef-sai-page-label").show();
+      } 
+      else {
+        $(".group-ef-sai-page-label").hide();
+      }
+    });
   }};
 })(jQuery);
 
@@ -14,6 +23,12 @@
     } 
     else {
       $("input#edit-field-ef-sai-label-en-0-value").prop("disabled", true);
+    }
+    if ($("input#edit-field-ef-observatory-und-13188").is(':checked')) {
+      $(".group-ef-sai-page-label").show();
+    } 
+    else {
+      $(".group-ef-sai-page-label").hide();
     }
   });
 })(jQuery);
