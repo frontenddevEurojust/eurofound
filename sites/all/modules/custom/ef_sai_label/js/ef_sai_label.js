daeaea11f4b5c01be1174459d7568bce82bef61d
MAREN LARREA <mlarrea@bilbomatica.es>
2018-06-26 12:47:40 +0200

    WEM-1081 [QA testing] SAI valie can be edited when 'override' box is not ticked FIX Fixed behavior for overiding SAI

--- sites/all/modules/custom/ef_sai_label/js/ef_sai_label.js
+++ sites/all/modules/custom/ef_sai_label/js/ef_sai_label.js
@@ -2,6 +2,10 @@
   Drupal.behaviors.efsailabel = {
   attach: function (context, settings) {
 
+    $('input#edit-field-ef-sai-label-override-und').once().change(function(){
+      $("input#edit-field-ef-sai-label-en-0-value").prop("disabled", !$(this).is(':checked'));
+    });
+
     var s2article = document.getElementsByClassName('node-ef_report-form');
     var s2nc = document.getElementsByClassName('node-ef_national_contribution-form');
     var s2car = document.getElementsByClassName('node-ef_comparative_analytical_report-form');
@@ -28,6 +32,13 @@
 
 (function ($) {
   $(document).ready(function(){
+
+    if ($("input#edit-field-ef-sai-label-override-und").is(':checked')) {
+      $("input#edit-field-ef-sai-label-en-0-value").prop("disabled", false);
+    } 
+    else {
+      $("input#edit-field-ef-sai-label-en-0-value").prop("disabled", true);
+    }
     
     var s2article = document.getElementsByClassName('node-ef_report-form');
     var s2nc = document.getElementsByClassName('node-ef_national_contribution-form');
