8b69499483003ab2103b41ca8db90c3f4282171c
jilorenzo <jilorenzo@bilbomatica.es>
2018-07-13 15:49:00 +0200

    WEM-949 Fix for modalityCode = NA

--- sites/all/modules/custom/ef_d3_dataexplorer/charts/d3.balance_care/d3.balance_care.js
+++ sites/all/modules/custom/ef_d3_dataexplorer/charts/d3.balance_care/d3.balance_care.js
@@ -232,7 +232,13 @@
 	{
 		var elementId = settingsArray.find(function(e)
 		{
-			return (e.chartID === chartName && e.modalityCode == modalityName);
+			console.log(e.modalityCode);
+			if(e.modalityCode != 'N/A'){
+				return (e.chartID === chartName && e.modalityCode == modalityName );
+			} else {
+				return (e.chartID === chartName && e.modalityCode === 'N/A' );
+			}
+			
 		});
 		if (elementId)
 		{
