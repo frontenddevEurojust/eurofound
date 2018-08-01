cab78c1a5d69f2fe859e23740dbecc8927bd77c0
Andreas Hennings <andreas@dqxtech.net>
2018-08-01 13:32:10 +0200

    AMEND ef_pleco_components.

--- sites/all/modules/custom/ef_pleco_components/layouts/ef_pleco_common/ef-pleco-common.tpl.php
+++ sites/all/modules/custom/ef_pleco_components/layouts/ef_pleco_common/ef-pleco-common.tpl.php
@@ -12,12 +12,14 @@
  *
  * @see ef_pleco_components_ds_layout_info()
  */
-$ds_content_classes = '';
-$right_classes = '';
+# $ds_content_classes = '';
+# $right_classes = '';
+$columns_classes = '';
 if ($ds_content !== '' && $right !== '') {
-  $classes .= '__columns';
-  $ds_content_classes .= ' large-9';
-  $right_classes .= ' large-3';
+  # $classes .= '__columns';
+  $columns_classes .= ' __columns';
+  # $ds_content_classes .= ' large-9';
+  # $right_classes .= ' large-3';
 }
 ?>
 <div<?php print $layout_attributes; ?> class="ef-pleco-common clearfix <?php print $classes;?> clearfix">
@@ -26,12 +28,12 @@ if ($ds_content !== '' && $right !== '') {
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
 
-  <div class="__columns">
-    <div class="__content<?php print $ds_content_classes; ?>">
+  <div class="__columns_area clearfix<?php print $columns_classes; ?>">
+    <div class="__content">
       <?php print $ds_content; ?>
     </div>
 
-    <aside class="__right<?php print $right_classes; ?>">
+    <aside class="__right">
       <?php print $right; ?>
     </aside>
   </div>
